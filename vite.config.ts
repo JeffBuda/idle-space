fbd96ee46da2074e928a5ce60ab81e3a991da25d
Jeff Buda <budajeff@gmail.com>
2024-11-13 00:58:24 -0500

    casing of repo name

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/scoresceror/', // This should match the repository name
+  base: '/Scoresceror/', // This should match the repository name
   plugins: [react()],
 });
